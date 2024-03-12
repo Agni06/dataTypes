805481a5add53f4c3dbfc583672516bede771971
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-12 11:20:16 +0530

    Create byte-int-double.java

---
+++ byte-int-double.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	byte c ;
+	int i = 258;
+	c = (byte) i;
+	System.out.println(c);
+	
+	double d = 234.33;
+	i = (int)d;
+	
+	System.out.print(i);
+	}
+}
